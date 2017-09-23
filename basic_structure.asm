cad3a609fd18e3066d0f33eba4b345cd46f84943
Taiga Nakayama <dora@dora-gt.jp>
2017-09-23 21:24:20 +0900

    backup

--- basic_structure.asm
+++ basic_structure.asm
@@ -3,8 +3,7 @@ global start
 section .text
 
 start:
-    int 3
-    cmp dword [esp], 5
+    cmp dword [esp], 2
     jl .if
     jge .else
 .if:
@@ -13,19 +12,35 @@ start:
 .else:
     call func_2
 .end_if:
-    push dword [esp]
+    push dword 0
     mov eax, 0x1
     sub esp, 4
     int 0x80
 
 func_1:
+    push dword hello_world_len
+    push dword hello_world
+    push dword 1
+    mov eax, 4
+    sub esp, 4
+    int 0x80
+    add esp, 16
     ret
 
 func_2:
+    push dword good_night_world_len
+    push dword good_night_world
+    push dword 1
+    mov eax, 4
+    sub esp, 4
+    int 0x80
+    add esp, 16
     ret
 
 section .data
-    hello_world db "Hello World!", 0
-    good_night_world db "Goodnight baby.", 0
+    hello_world db "Hello World!", 10
+    hello_world_len equ $ - hello_world
+    good_night_world db "Goodnight baby.", 10
+    good_night_world_len equ $ - good_night_world
 
 section .bss
