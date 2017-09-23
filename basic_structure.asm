b3284ad03612ee8cbaab1fcaf38ff89962d3382f
Taiga Nakayama <dora@dora-gt.jp>
2017-09-23 20:37:02 +0900

    backup

--- basic_structure.asm
+++ basic_structure.asm
@@ -4,10 +4,28 @@ section .text
 
 start:
     int 3
+    cmp dword [esp], 5
+    jl .if
+    jge .else
+.if:
+    call func_1
+    jmp .end_if
+.else:
+    call func_2
+.end_if:
+    push dword [esp]
+    mov eax, 0x1
+    sub esp, 4
+    int 0x80
 
+func_1:
+    ret
 
+func_2:
+    ret
 
 section .data
-    hello_world db  "Hello World!", 0
+    hello_world db "Hello World!", 0
+    good_night_world db "Goodnight baby.", 0
 
 section .bss
